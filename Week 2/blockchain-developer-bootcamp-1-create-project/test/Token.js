2161b8edbbcfdd2722e5e0fab0a9d82b39477d56
proconectach38 <contact@proconecta.com>
2023-10-12 17:21:05 +0300

    2. Token Transfers

--- Week 2/blockchain-developer-bootcamp-1-create-project/test/Token.js
+++ Week 2/blockchain-developer-bootcamp-1-create-project/test/Token.js
@@ -4,13 +4,14 @@ const { expect } = require('chai');
 const tokens = (number) => ethers.utils.parseUnits(number.toString(), 'ether')
 
 describe('Token', () => {
-    let token, accounts, deployer;
+    let token, accounts, deployer, receiver;
 
     beforeEach(async () => {
         const Token = await ethers.getContractFactory('Token');
         token = await Token.deploy('Meme Ether', 'MTH', '1000000');
         accounts = await ethers.getSigners();
         deployer = accounts[0];
+        receiver = accounts[1];
     })
 
     describe('Deployment', () => {
@@ -37,6 +38,46 @@ describe('Token', () => {
 
         it('assigns totalSupply to deployer', async () => {
             expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+        })
+    })
+
+    describe('Sending Tokens', () => {
+        let amount, transaction, result;
+
+        describe('Successfully', () => {
+            beforeEach(async () => {
+                amount = tokens(100);
+                transaction = await token.connect(deployer).transfer(receiver.address, amount);
+                result = await transaction.wait();
+            })
+    
+            it('transfers token balances', async () => {
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900));
+                expect(await token.balanceOf(receiver.address)).to.equal(amount);
+            })
+    
+            it('emits a transfer event', async () => {
+                const firstEvent = result.events[0];
+                expect(await firstEvent.event).to.equal('Transfer');
+                
+                const args = firstEvent.args;
+                expect(args.from).to.equal(deployer.address);
+                expect(args.to).to.equal(receiver.address);
+                expect(args.value).to.equal(amount);
+            })
+        })
+        
+        describe('Fails', () => {
+            it('when insufficient balances', async () => {
+                // Transfer more token than deployer has - 10M
+                const invalidAmount = tokens(1000000000);
+                await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted;
+            })
+
+            it('when invalid recipient', async () => {
+                const amount = tokens(100);
+                await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted;
+            })
         })
     })
 })
