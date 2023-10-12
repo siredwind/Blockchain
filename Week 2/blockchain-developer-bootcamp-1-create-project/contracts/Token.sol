e0f37efb09356c3388e8ee5d64ce0a62573c5de5
proconectach38 <contact@proconecta.com>
2023-10-12 14:52:01 +0300

    2. Testing Contracts

--- Week 2/blockchain-developer-bootcamp-1-create-project/contracts/Token.sol
+++ Week 2/blockchain-developer-bootcamp-1-create-project/contracts/Token.sol
@@ -4,5 +4,15 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "Mihai's Token";
+    string public name = "Meme Ether";
+    string public symbol = "MTH";
+    uint256 public decimals = 18;
+    uint256 public totalSupply = (10 ** 6) * (10 ** decimals);
+
+    constructor(string memory _name, string memory _symbol, uint256 _decimals, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+    }
 }
