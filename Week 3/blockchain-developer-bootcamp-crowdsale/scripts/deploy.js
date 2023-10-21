5c7ab5f94a315ce191725d7b4f41780bbc5c8904
proconectach38 <contact@proconecta.com>
2023-10-21 23:38:24 +0300

    Week 3 - deploy smart contracts

--- Week 3/blockchain-developer-bootcamp-crowdsale/scripts/deploy.js
+++ Week 3/blockchain-developer-bootcamp-crowdsale/scripts/deploy.js
@@ -7,22 +7,30 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NAME = 'Meme Ether';
+  const SYMBOL = 'MTH';
+  const MAX_SUPPLY = '1000000';
+  const PRICE = hre.ethers.utils.parseUnits('0.025', 'ether');
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // Deploy Token
+  const Token = await hre.ethers.getContractFactory('Token');
+  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await token.deployed();
+  console.log(`Token deployed to: ${token.address}\n`);
 
-  await lock.waitForDeployment();
+  // Deploy Crowdsale
+  const Crowdsale = await hre.ethers.getContractFactory('Crowdsale');
+  let crowdsale = await Crowdsale.deploy(token.address, PRICE, MAX_SUPPLY);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await crowdsale.deployed();
+  console.log(`Crowdsale deployed to: ${crowdsale.address}\n`);
+
+  // Send tokens to crowdsale
+  const transaction = await token.transfer(crowdsale.address, hre.ethers.utils.parseUnits(MAX_SUPPLY, 'ether'));
+  await transaction.wait();
+
+  console.log(`Tokens transferred to Crowdsale.\n`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
