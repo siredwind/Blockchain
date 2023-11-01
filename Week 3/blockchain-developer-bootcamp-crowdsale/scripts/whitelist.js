7a8f5820f3a3449f164a7475c475b4ccee9b7adb
proconectach38 <contact@proconecta.com>
2023-11-01 15:06:14 +0200

    Week 3 - Created proper whitelist script

--- Week 3/blockchain-developer-bootcamp-crowdsale/scripts/whitelist.js
+++ Week 3/blockchain-developer-bootcamp-crowdsale/scripts/whitelist.js
@@ -1,45 +1,44 @@
-const ethers = require("ethers");
-const CROWDSALE_ABI = require('../src/abis/Crowdsale.json');
+const hre = require("hardhat");
 const config = require('../src/config.json');
 
-const usersToAddToWhitelist = [
-    '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-    '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
-    // Add more user addresses as needed
-  ];
-
-const CHAIN_ID = 31337;
-const CROWDSALE_ADDRESS = config[CHAIN_ID].crowdsale.address;
-const NETWORK_URL = 'http:localhost:8545';
-const OWNER_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
-
 async function main() {
-    addToWhitelist();
-}   
+  console.log(`Fetching accounts & network... \n`);
+  const accounts = await hre.ethers.getSigners();
+  const deployer = accounts[0];
+  const user1 = accounts[1];
+  const user2 = accounts[2];
+  const user3 = accounts[3];
 
-async function addToWhitelist() {
-    // Connect to the Ethereum network
-    const provider = new ethers.providers.JsonRpcProvider(NETWORK_URL);
+  let transaction;
 
-    // Connect to the Crowdsale contract
-    const wallet = new ethers.Wallet(OWNER_PRIVATE_KEY, provider);
-    const crowdsale = new ethers.Contract(CROWDSALE_ADDRESS, CROWDSALE_ABI, wallet);
-    console.log(crowdsale)
-    for (const userAddress of usersToAddToWhitelist) {
-      try {
-        // Call the addToWhitelist function to add the user to the whitelist
-        const tx = await crowdsale.addToWhitelist(userAddress);
-        await tx.wait();
-        console.log(`User ${userAddress} added to the whitelist.`);
-      } catch (error) {
-        console.error(`Failed to add user ${userAddress} to the whitelist: ${error.message}`);
-      }
-    }
-  }
-  
+  // Fetch network
+  const { chainId } = await hre.ethers.provider.getNetwork();
+
+  console.log(`Fetching token and whitelisting accounts... \n`);
+
+  // Fetch deployed crowdsale
+  const crowdsale = await hre.ethers.getContractAt('Crowdsale', config[chainId].crowdsale.address);
+  console.log(`Crowdsale fetched: ${crowdsale.address}\n`);
+
+  // Whitelist accounts
+  transaction = await crowdsale.connect(deployer).addToWhitelist(user1);
+  await transaction.wait();
+  console.log(`User ${user1} added to the whitelist.\n`);
+
+  transaction = await crowdsale.connect(deployer).addToWhitelist(user2);
+  await transaction.wait();
+  console.log(`User ${user2} added to the whitelist.\n`);
+
+  transaction = await crowdsale.connect(deployer).addToWhitelist(user3);
+  await transaction.wait();
+  console.log(`User ${user3} added to the whitelist.\n`);
+
+  console.log(`Finished.\n`);
+}
+
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
+  console.error(error);
+  process.exitCode = 1;
+});
