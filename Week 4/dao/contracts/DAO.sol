faf731e04e8238b51b6fa13825d6210d914c98d1
proconectach38 <contact@proconecta.com>
2023-11-03 15:22:50 +0200

    Week 4 - Use openzeppelin package

--- Week 4/dao/contracts/DAO.sol
+++ Week 4/dao/contracts/DAO.sol
@@ -1,13 +1,16 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "hardhat/console.sol";
 import "./Token.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DAO {
+    IERC20 public token;
     address owner;
-    Token public token;
     uint256 public quorum;
+
+    enum PaymentType { Ether, Token }
 
     struct Proposal {
         uint256 id;
@@ -16,17 +19,19 @@ contract DAO {
         address payable recipient;
         uint256 votes;
         bool finalized;
+        PaymentType paymentType;
     }
 
     uint256 public proposalCount;
     mapping(uint256 => Proposal) public proposals;
     mapping(address => mapping(uint256 => bool)) public votes;
+    mapping(address => uint256) public tokenBalances;
 
-    event Propose(uint id, uint256 amount, address recipient, address creator);
+    event Propose(uint id, uint256 amount, address recipient, address creator, PaymentType paymentType);
     event Vote(uint256 id, address investor);
     event Finalize(uint256 id);
 
-    constructor(Token _token, uint256 _quorum) {
+    constructor(IERC20 _token, uint256 _quorum) {
         owner = msg.sender;
         token = _token;
         quorum = _quorum;
@@ -44,10 +49,16 @@ contract DAO {
     function createProposal(
         string memory _name,
         uint256 _amount,
-        address payable _recipient
+        address payable _recipient,
+        PaymentType _paymentType
     ) external onlyInvestor {
         require(bytes(_name).length > 0, "Proposal must have a description");
-        require(address(this).balance >= _amount);
+
+        if (_paymentType == PaymentType.Ether) {
+            require(address(this).balance >= _amount, "Insufficient Ether balance");
+        } else if (_paymentType == PaymentType.Token) {
+            require(token.balanceOf(address(this)) >= _amount, "Insufficient token balance");
+        }
 
         proposalCount++;
 
@@ -58,10 +69,11 @@ contract DAO {
             _amount,
             _recipient,
             0,
-            false
+            false,
+            _paymentType
         );
 
-        emit Propose(proposalCount, _amount, _recipient, msg.sender);
+        emit Propose(proposalCount, _amount, _recipient, msg.sender, _paymentType);
     }
 
     // Vote on proposal
@@ -122,14 +134,34 @@ contract DAO {
             "must reach quorum to finalize proposal"
         );
 
-        // Check that the contract has enough Ether
-        require(address(this).balance >= proposal.amount);
+        if (proposal.paymentType == PaymentType.Ether) {
+            // Check contract has enough Ether
+            require(address(this).balance >= proposal.amount, "Insufficient Ether balance");
 
-        // Transfer funds
-        (bool sent, ) = proposal.recipient.call{value: proposal.amount}("");
-        require(sent);
+            // Transfer funds
+            (bool sent, ) = proposal.recipient.call{value: proposal.amount}("");
+            require(sent, "Failed to transfer Ether");
+        } else if (proposal.paymentType == PaymentType.Token) {
+            // Check that the contract has enough Tokens
+            require(hasEnoughTokens(address(this), proposal.amount), "Insufficient contract token balance");
+
+            // Transfer funds
+            require(token.transfer(proposal.recipient, proposal.amount), "Token transfer failed");
+        }
 
         // Emit events
         emit Finalize(_id);
+    }
+
+    // Update the balance when tokens are transferred in
+    function depositTokens(uint256 amount) external {
+        // Implement the transfer function from the token contract
+        require(token.transfer(address(this), amount), "Token transfer failed");
+        tokenBalances[msg.sender] += amount;
+    }
+
+    // Check token balance
+    function hasEnoughTokens(address user, uint256 amount) internal view returns (bool) {
+        return tokenBalances[user] >= amount;
     }
 }
