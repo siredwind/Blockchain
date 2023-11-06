c5a2fe1a9c5858736c20b98abe972f5a0be39f1a
proconectach38 <contact@proconecta.com>
2023-11-06 13:52:23 +0200

    Week 5 - Create website for minting

--- Week-5/nft/src/components/App.js
+++ Week-5/nft/src/components/App.js
@@ -1,59 +1,110 @@
 import { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
+import Countdown from 'react-countdown'
 import { ethers } from 'ethers'
+
+// IMG
+import preview from '../preview.png';
 
 // Components
 import Navigation from './Navigation';
 import Loading from './Loading';
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import NFT_ABI from '../abis/NFT.json';
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from '../config.json';
+import Data from './Data';
+import Mint from './Mint';
 
 function App() {
-  const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(0)
+  const [provider, setProvider] = useState(null);
+  const [nft, setNFT] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [revealTime, setRevealTime] = useState(null);
+  const [maxSupply, setMaxSupply] = useState(0);
+  const [totalSupply, setTotalSupply] = useState(0);
+  const [cost, setCost] = useState(0);
+  const [balance, setBalance] = useState(0);
 
   const [isLoading, setIsLoading] = useState(true)
 
   const loadBlockchainData = async () => {
     // Initiate provider
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    // Initiate contract
+    const nft = new ethers.Contract(config[31337].nft.address, NFT_ABI, provider);
+    setNFT(nft);
 
     // Fetch accounts
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
 
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
+    // Fetch Countdown
+    const allowMintingOn = await nft.allowMintingOn();
+    setRevealTime(allowMintingOn.toString() + '000');
 
-    setIsLoading(false)
+    // Fetch maxSupply
+    setMaxSupply(await nft.maxSupply());
+
+    // Fetch totalSupply
+    setTotalSupply(await nft.totalSupply());
+
+    // Fetch cost
+    setCost(await nft.cost());
+
+    // Fetch balanceOf
+    setBalance(await nft.balanceOf(account));
+
+    setIsLoading(false);
   }
 
   useEffect(() => {
     if (isLoading) {
-      loadBlockchainData()
+      loadBlockchainData();
     }
   }, [isLoading]);
 
-  return(
+  return (
     <Container>
       <Navigation account={account} />
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+      <h1 className='my-4 text-center'>Dapp Punks</h1>
 
       {isLoading ? (
         <Loading />
       ) : (
-        <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
-          <p className='text-center'>Edit App.js to add your code here.</p>
-        </>
+        <Row>
+          <Col>
+            {balance > 0
+              ? (
+                <div className='text-center'>
+                  <img
+                    src={`https://gateway.pinata.cloud/ipfs/QmQPEMsfd1tJnqYPbnTQCjoa8vczfsV1FmqZWgRdNQ7z3g/${balance.toString()}.png`}
+                    alt="Open Punk"
+                    width="400px"
+                    height="400px"
+                  />
+                </div>
+              )
+              : (
+                <img src={preview} alt="" />
+              )
+            }
+          </Col>
+          <Col>
+            <div className='my-4 text-center'>
+              <Countdown className='h2' date={parseInt(revealTime)} />
+            </div>
+
+            <Data {...{ maxSupply, totalSupply, cost, balance }} />
+            <Mint {...{ provider, nft, cost, setIsLoading }} />
+          </Col>
+        </Row>
       )}
     </Container>
   )
