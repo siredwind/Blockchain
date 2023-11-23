e0cc141c5b7a6d83557e01b2ccffb6edde88aa70
proconectach38 <contact@proconecta.com>
2023-11-23 11:36:49 +0200

    Week 8 - init project

---
+++ Week-8/amm/scripts/deploy.js
@@ -0,0 +1,33 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const Token = await hre.ethers.getContractFactory('Token')
+
+  // Deploy Token 2
+  let dapp = await Token.deploy('Dapp Token', 'DAPP', '1000000')
+  await dapp.deployed()
+  console.log(`Dapp Token deployed to: ${dapp.address}\n`)
+
+  // Deploy Token 2
+  let usd = await Token.deploy('USD Token', 'USD', '1000000')
+  await usd.deployed()
+  console.log(`USD Token deployed to: ${usd.address}\n`)
+
+  // Deploy AMM
+  const AMM = await hre.ethers.getContractFactory('AMM')
+  const amm = await AMM.deploy(dapp.address, usd.address)
+  console.log(`AMM Contract deployed to: ${amm.address}`)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
